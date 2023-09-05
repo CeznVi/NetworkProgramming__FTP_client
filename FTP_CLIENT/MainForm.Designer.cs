8ca60be302b4310fc7ae3fbd1c084d0704546b86
Viktor Velychko <Cezn.Vi@gmail.com>
2023-09-05 23:22:59 +0300

    W I P

--- FTP_CLIENT/MainForm.Designer.cs
+++ FTP_CLIENT/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FTP_CLIENT
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             menuStrip1 = new MenuStrip();
             fileToolStripMenuItem = new ToolStripMenuItem();
             statusStrip1 = new StatusStrip();
@@ -44,8 +46,12 @@
             button_rename = new Button();
             label3 = new Label();
             textBox_fname = new TextBox();
+            contextMenuStrip_forTree = new ContextMenuStrip(components);
+            удалитьToolStripMenuItem = new ToolStripMenuItem();
+            button_download = new Button();
             menuStrip1.SuspendLayout();
             statusStrip1.SuspendLayout();
+            contextMenuStrip_forTree.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -82,7 +88,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(220, 24);
+            label1.Location = new Point(247, 24);
             label1.Name = "label1";
             label1.Size = new Size(40, 15);
             label1.TabIndex = 2;
@@ -90,7 +96,7 @@
             // 
             // textBox_login
             // 
-            textBox_login.Location = new Point(266, 21);
+            textBox_login.Location = new Point(293, 21);
             textBox_login.Name = "textBox_login";
             textBox_login.Size = new Size(100, 23);
             textBox_login.TabIndex = 3;
@@ -98,28 +104,29 @@
             // 
             // textBox_pasw
             // 
-            textBox_pasw.Location = new Point(411, 21);
+            textBox_pasw.Location = new Point(465, 21);
             textBox_pasw.Name = "textBox_pasw";
-            textBox_pasw.Size = new Size(100, 23);
+            textBox_pasw.Size = new Size(128, 23);
             textBox_pasw.TabIndex = 5;
             textBox_pasw.Text = "ftp";
+            textBox_pasw.UseSystemPasswordChar = true;
             // 
             // label_pass
             // 
             label_pass.AutoSize = true;
-            label_pass.Location = new Point(372, 24);
+            label_pass.Location = new Point(399, 24);
             label_pass.Name = "label_pass";
-            label_pass.Size = new Size(33, 15);
+            label_pass.Size = new Size(60, 15);
             label_pass.TabIndex = 4;
-            label_pass.Text = "Pass:";
+            label_pass.Text = "Password:";
             // 
             // textBox_host
             // 
-            textBox_host.Location = new Point(48, 21);
+            textBox_host.Location = new Point(48, 24);
             textBox_host.Name = "textBox_host";
-            textBox_host.Size = new Size(166, 23);
+            textBox_host.Size = new Size(193, 23);
             textBox_host.TabIndex = 7;
-            textBox_host.Text = "ftp://127.0.0.1/";
+            textBox_host.Text = "ftp://127.0.0.1";
             // 
             // label2
             // 
@@ -132,11 +139,11 @@
             // 
             // button_connect
             // 
-            button_connect.Location = new Point(607, 20);
+            button_connect.Location = new Point(607, 21);
             button_connect.Name = "button_connect";
-            button_connect.Size = new Size(181, 23);
+            button_connect.Size = new Size(181, 38);
             button_connect.TabIndex = 8;
-            button_connect.Text = "Connect/Update";
+            button_connect.Text = "Подключится / Обновить";
             button_connect.UseVisualStyleBackColor = true;
             button_connect.Click += button_connect_Click;
             // 
@@ -144,22 +151,24 @@
             // 
             treeView_server.Location = new Point(5, 50);
             treeView_server.Name = "treeView_server";
+            treeView_server.PathSeparator = "/";
             treeView_server.Size = new Size(588, 362);
             treeView_server.TabIndex = 9;
+            treeView_server.AfterSelect += treeView_server_AfterSelect;
             // 
             // button_Delete
             // 
-            button_Delete.Location = new Point(607, 62);
+            button_Delete.Location = new Point(607, 101);
             button_Delete.Name = "button_Delete";
-            button_Delete.Size = new Size(181, 23);
+            button_Delete.Size = new Size(181, 30);
             button_Delete.TabIndex = 10;
-            button_Delete.Text = "Delete";
+            button_Delete.Text = "Удалить";
             button_Delete.UseVisualStyleBackColor = true;
             button_Delete.Click += button_Delete_Click;
             // 
             // button_rename
             // 
-            button_rename.Location = new Point(607, 155);
+            button_rename.Location = new Point(607, 212);
             button_rename.Name = "button_rename";
             button_rename.Size = new Size(181, 23);
             button_rename.TabIndex = 11;
@@ -170,7 +179,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(623, 108);
+            label3.Location = new Point(607, 156);
             label3.Name = "label3";
             label3.Size = new Size(87, 15);
             label3.TabIndex = 12;
@@ -178,16 +187,40 @@
             // 
             // textBox_fname
             // 
-            textBox_fname.Location = new Point(607, 126);
+            textBox_fname.Location = new Point(607, 183);
             textBox_fname.Name = "textBox_fname";
             textBox_fname.Size = new Size(116, 23);
             textBox_fname.TabIndex = 13;
             // 
-            // Form1
+            // contextMenuStrip_forTree
+            // 
+            contextMenuStrip_forTree.Items.AddRange(new ToolStripItem[] { удалитьToolStripMenuItem });
+            contextMenuStrip_forTree.Name = "contextMenuStrip";
+            contextMenuStrip_forTree.Size = new Size(119, 26);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            удалитьToolStripMenuItem.Size = new Size(118, 22);
+            удалитьToolStripMenuItem.Text = "Удалить";
+            удалитьToolStripMenuItem.Click += удалитьToolStripMenuItem_Click;
+            // 
+            // button_download
+            // 
+            button_download.Location = new Point(607, 65);
+            button_download.Name = "button_download";
+            button_download.Size = new Size(181, 30);
+            button_download.TabIndex = 14;
+            button_download.Text = "Скачать";
+            button_download.UseVisualStyleBackColor = true;
+            button_download.Click += button_download_Click;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button_download);
             Controls.Add(textBox_fname);
             Controls.Add(label3);
             Controls.Add(button_rename);
@@ -202,13 +235,15 @@
             Controls.Add(label1);
             Controls.Add(statusStrip1);
             Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
-            Name = "Form1";
+            Name = "MainForm";
             Text = "FTP3000";
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
+            contextMenuStrip_forTree.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -231,5 +266,8 @@
         private Button button_rename;
         private Label label3;
         private TextBox textBox_fname;
+        private ContextMenuStrip contextMenuStrip_forTree;
+        private ToolStripMenuItem удалитьToolStripMenuItem;
+        private Button button_download;
     }
 }
---
+++ FTP_CLIENT/MainForm.cs
@@ -0,0 +1,413 @@
+using Microsoft.VisualBasic.FileIO;
+using System.Net;
+
+namespace FTP_CLIENT
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            treeView_server.ContextMenuStrip = contextMenuStrip_forTree;
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            UpdateView();
+        }
+
+
+        ///Оновить древо
+        private void UpdateView()
+        {
+            treeView_server.Nodes.Clear();
+            TreeNode rootNode = new TreeNode(textBox_host.Text);
+            treeView_server.Nodes.Add(rootNode);
+
+            UpdTreeView(textBox_host.Text, textBox_login.Text, textBox_pasw.Text, "", rootNode);
+        }
+
+        /// <summary>
+        /// Рекурсионный метод для обновление дерева и данных вложености
+        /// </summary>
+        private void UpdTreeView(string ftpServer, string ftpUsername, string ftpPassword, string ftpPath, TreeNode parentNode)
+        {
+            FtpWebRequest ftpWebRequest = (FtpWebRequest)WebRequest.Create(ftpServer + ftpPath);
+            ftpWebRequest.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            try
+            {
+                FtpWebResponse ftpResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                Stream streamReaderResponse = ftpResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(streamReaderResponse);
+
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (parts.Length >= 9)
+                    {
+
+                        string itemName = string.Empty;
+
+                        for (int i = 8; i < parts.Length; i++)
+                        {
+                            if (parts.Length - 8 == 0)
+                                itemName += parts[i];
+                            else
+                            {
+                                if (i < parts.Length - 1)
+                                    itemName += parts[i] + " ";
+                                else
+                                    itemName += parts[i];
+                            }
+                        }
+
+                        TreeNode itemNode = new TreeNode(itemName);
+
+                        if (line.StartsWith("d"))
+                        {
+                            itemNode.Tag = "Directory";
+                            UpdTreeView(ftpServer, ftpUsername, ftpPassword, ftpPath + "/" + itemName, itemNode);
+                        }
+                        else
+                        {
+                            string fileExtension = Path.GetExtension(itemName);
+
+                            if (!string.IsNullOrEmpty(fileExtension))
+                            {
+                                itemNode.Tag = "File";
+                            }
+                        }
+                        parentNode.Nodes.Add(itemNode);
+                    }
+                }
+            }
+            catch (WebException webx)
+            {
+                MessageBox.Show("Ошибка при подключении к FTP-серверу: \n" + webx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        ///--------------------------------------------------------Контектсное меню вызов cобытий
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CheckSelectedItemTree())
+                DeleteSelectedItem();
+
+        }
+        ///////--------------------------------------------------------------------END
+
+        ///--------------------------------------------------------Кнопки вызов cобытий
+        
+        //УДАЛИТЬ
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+            if (CheckSelectedItemTree())
+                DeleteSelectedItem();
+        }
+        //СКАЧАТЬ
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            if (CheckSelectedItemTree())
+                DownloadSelectedItem();
+        }
+        ///////--------------------------------------------------------------------END
+
+        ///--------------------------------------------------------МЕТОДЫ реализации действий
+
+        //******************        УДАЛЕНИЕ //
+        private void DeleteSelectedItem()
+        {
+            if (isSelectedItemDirectory())
+            {
+                DeleteDirectory();
+            }
+            else if (isSelectedItemFile())
+            {
+                DeleteFile();
+            }
+            else
+                return;
+        }
+        private void DeleteDirectory()
+        {
+            string deletePath = treeView_server.SelectedNode.FullPath;
+
+            try
+            {
+                foreach (TreeNode item in treeView_server.SelectedNode.Nodes)
+                {
+                    DeleteFileFromLink(item.FullPath);
+                }
+
+                Uri uriDeleteFile = new Uri(deletePath);
+
+                if (uriDeleteFile.Scheme != Uri.UriSchemeFtp)
+                {
+                    MessageBox.Show("Некоректный адресс");
+                    return;
+                }
+
+                FtpWebRequest ftpWebRequestDeleteFile = (FtpWebRequest)WebRequest.Create(uriDeleteFile);
+                ftpWebRequestDeleteFile.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+                ftpWebRequestDeleteFile.Method = WebRequestMethods.Ftp.RemoveDirectory;
+
+                FtpWebResponse ftpWebResponseDeleteFile = (FtpWebResponse)ftpWebRequestDeleteFile.GetResponse();
+
+                toolStripStatusLabel.Text = ftpWebResponseDeleteFile.StatusCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            UpdateView();
+        }
+        private void DeleteFile()
+        {
+            string deletePath = treeView_server.SelectedNode.FullPath;
+
+            try
+            {
+                Uri uriDeleteFile = new Uri(deletePath);
+
+                if (uriDeleteFile.Scheme != Uri.UriSchemeFtp)
+                {
+                    MessageBox.Show("Некоректный адресс");
+                    return;
+                }
+
+                FtpWebRequest ftpWebRequestDeleteFile = (FtpWebRequest)WebRequest.Create(uriDeleteFile);
+                ftpWebRequestDeleteFile.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+                ftpWebRequestDeleteFile.Method = WebRequestMethods.Ftp.DeleteFile;
+
+                FtpWebResponse ftpWebResponseDeleteFile = (FtpWebResponse)ftpWebRequestDeleteFile.GetResponse();
+
+                toolStripStatusLabel.Text = ftpWebResponseDeleteFile.StatusCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            UpdateView();
+        }
+        private void DeleteFileFromLink(string link)
+        {
+            try
+            {
+                Uri uriDeleteFile = new Uri(link);
+
+                if (uriDeleteFile.Scheme != Uri.UriSchemeFtp)
+                {
+                    MessageBox.Show("Некоректный адресс");
+                    return;
+                }
+
+                FtpWebRequest ftpWebRequestDeleteFile = (FtpWebRequest)WebRequest.Create(uriDeleteFile);
+                ftpWebRequestDeleteFile.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+                ftpWebRequestDeleteFile.Method = WebRequestMethods.Ftp.DeleteFile;
+
+                FtpWebResponse ftpWebResponseDeleteFile = (FtpWebResponse)ftpWebRequestDeleteFile.GetResponse();
+
+                toolStripStatusLabel.Text = ftpWebResponseDeleteFile.StatusCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //******************        СКАЧАТЬ //
+        private void DownloadSelectedItem()
+        {
+            if (isSelectedItemFile())
+            {
+                DownloadFile();
+            }
+            else
+                return;
+        }
+
+        private void DownloadFile()
+        {
+            string downloadPath = treeView_server.SelectedNode.FullPath;
+
+            try
+            {
+
+                Uri uriDownloadFile = new Uri(downloadPath);
+
+                if (uriDownloadFile.Scheme != Uri.UriSchemeFtp)
+                {
+                    MessageBox.Show("Некоректный адресс");
+                    return;
+                }
+
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                folderBrowserDialog.UseDescriptionForTitle = true;
+                folderBrowserDialog.Description = "Выбирите папку в которую необходимо скачать файл";
+                string pathToSaveFile;
+                
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pathToSaveFile = folderBrowserDialog.SelectedPath;
+                    pathToSaveFile += "\\" + treeView_server.SelectedNode.Text;
+                }
+                else
+                    return;
+
+                FtpWebRequest ftpWebRequestDownloadFile = (FtpWebRequest)WebRequest.Create(uriDownloadFile);
+                ftpWebRequestDownloadFile.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+                ftpWebRequestDownloadFile.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                FtpWebResponse ftpWebResponseDownloadFile = (FtpWebResponse)ftpWebRequestDownloadFile.GetResponse();
+
+
+                Stream streamDownloadFile = ftpWebResponseDownloadFile.GetResponseStream();
+
+                List<byte> downloadFileList = new List<byte>();
+
+                int data = 0;
+                while ((data = streamDownloadFile.ReadByte()) != -1)
+                {
+                    downloadFileList.Add((byte)data);
+                }
+
+                File.WriteAllBytes(pathToSaveFile, downloadFileList.ToArray());
+
+
+                toolStripStatusLabel.Text = ftpWebResponseDownloadFile.StatusDescription;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            UpdateView();
+        }
+
+
+
+        ///////--------------------------------------------------------------------END
+
+        ///--------------------------------------------------------МЕТОДЫ различных проверок
+
+        ///Проверить выделенный елемент ( не является ли он пустым, и есть ли он папкой либо файлом)
+        private bool CheckSelectedItemTree()
+        {
+            if (treeView_server.SelectedNode != null)
+            {
+                if (treeView_server.SelectedNode.Tag != null)
+                {
+                    if ((treeView_server.SelectedNode.Tag.ToString() == "File" || treeView_server.SelectedNode.Tag.ToString() == "Directory"))
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        private bool isSelectedItemDirectory()
+        {
+            if (treeView_server.SelectedNode.Tag.ToString() == "Directory")
+                return true;
+            else
+                return false;
+        }
+        private bool isSelectedItemFile()
+        {
+            if (treeView_server.SelectedNode.Tag.ToString() == "File")
+                return true;
+            else
+                return false;
+        }
+        ///////--------------------------------------------------------------------END
+
+
+
+
+        /// ПЕРЕДЕЛАТЬ!!
+        //private void button_Delete_Click(object sender, EventArgs e)
+        //{
+        //    if (treeView_server.SelectedNode != null)
+        //    {
+        //        Uri uriDeleteFile = new Uri(textBox_host.Text + treeView_server.SelectedNode.Text);
+
+        //        if (uriDeleteFile.Scheme != Uri.UriSchemeFtp)
+        //        {
+        //            MessageBox.Show("Некоректный адресс");
+        //            return;
+        //        }
+
+        //        FtpWebRequest ftpWebRequestDeleteFile = (FtpWebRequest)WebRequest.Create(uriDeleteFile);
+        //        ftpWebRequestDeleteFile.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+        //        ftpWebRequestDeleteFile.Method = WebRequestMethods.Ftp.DeleteFile;
+
+        //        FtpWebResponse ftpWebResponseDeleteFile = (FtpWebResponse)ftpWebRequestDeleteFile.GetResponse();
+
+        //        MessageBox.Show($"Status description: {ftpWebResponseDeleteFile.StatusDescription}");
+
+
+        //        //Connect();
+        //    }
+
+        //}
+
+        private void button_rename_Click(object sender, EventArgs e)
+        {
+            if (treeView_server.SelectedNode != null)
+            {
+
+                Uri uriRenameFile = new Uri(textBox_host.Text + treeView_server.SelectedNode.Text);
+
+
+                if (uriRenameFile.Scheme != Uri.UriSchemeFtp)
+                {
+                    MessageBox.Show("Некоректный адресс");
+                    return;
+                }
+
+                try
+                {
+                    FtpWebRequest ftpWebRequestRename = (FtpWebRequest)WebRequest.Create(uriRenameFile);
+                    ftpWebRequestRename.Credentials = new NetworkCredential(textBox_login.Text, textBox_pasw.Text);
+                    ftpWebRequestRename.Method = WebRequestMethods.Ftp.Rename;
+
+
+                    ftpWebRequestRename.RenameTo = textBox_fname.Text + treeView_server.SelectedNode.Text;
+
+
+                    FtpWebResponse ftpWebResponseRenameFile = (FtpWebResponse)ftpWebRequestRename.GetResponse();
+
+                    MessageBox.Show($"Status description: {ftpWebResponseRenameFile.StatusDescription}");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void treeView_server_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+
+    }
+}
